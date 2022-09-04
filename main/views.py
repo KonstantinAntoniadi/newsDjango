753c7f7d339fdb12b1cbc42f3c7d942e69e7cb6f
Konstantin Antoniadi <kos-antoniadi@yandex.ru>
2022-09-04 10:47:11 +0300

    add urls for main

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(requset):
+    return HttpResponse("<h4>Check work</h4>")
+
+def about(request):
+    return HttpResponse("<h4>About</h4>")
+
+def lessons(request):
+    return HttpResponse("<h1>Lessons</h1>")
+
+def students(request):
+    return HttpResponse("<h2><font color=""#fa8e47"">Students</font></h2>")
