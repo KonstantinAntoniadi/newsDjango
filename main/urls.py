fc5c0966112a14d3e5494f3e3cea7bbc422d707d
Konstantin Antoniadi <kos-antoniadi@yandex.ru>
2022-09-04 11:21:48 +0300

    add templates

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('about', views.about),
-    path('lessons', views.lessons),
-    path('students', views.students)
+    # path('lessons', views.lessons),
+    # path('students', views.students)
 ]
--- main/views.py
+++ main/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(requset):
-    return HttpResponse("<h4>Check work</h4>")
+    return render(requset, 'main/index.html')
 
 def about(request):
-    return HttpResponse("<h4>About</h4>")
+    return render(request, 'main/about.html')
 
-def lessons(request):
-    return HttpResponse("<h1>Lessons</h1>")
-
-def students(request):
-    return HttpResponse("<h2><font color=""#fa8e47"">Students</font></h2>")
+# def lessons(request):
+#     return HttpResponse("<h1>Lessons</h1>")
+#
+# def students(request):
+#     return HttpResponse("<h2><font color=""#fa8e47"">Students</font></h2>")
