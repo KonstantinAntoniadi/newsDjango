d8b5e7ad051cd86d80920f74a0077341c8752e28
Konstantin Antoniadi <kos-antoniadi@yandex.ru>
2022-09-05 23:31:39 +0300

    add news page

--- news/views.py
+++ news/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def news_home(request):
+    data ={
+        'title': 'News on page'
+    }
+    return render(request, 'news/news.html', data)
