b1c44f134ade7d801372d73fe728defda8957960
Konstantin Antoniadi <kos-antoniadi@yandex.ru>
2022-09-05 23:38:41 +0300

    add articles model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+    title = models.CharField('Title', max_length=50, default='21')
+    anons = models.CharField('Anons', max_length=250)
+    fullt_text = models.TextField('Article')
+    date = models.DateTimeField('Date publication')
+
+    def __str__(self):
+        return self.title
